57a79d50824f608c6074b2b7e97e348df89cd6ff
andyvy <thienvy95@gmail.com>
2022-05-22 14:02:06 +0700

    create coach detail page

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,3 +1,78 @@
 <template>
-    <h3>Coach Detail</h3>
+  <div>
+    <section>
+      <base-card>
+        <h2>{{ fullName }}</h2>
+        <h3>${{ rate }}/hour</h3>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <header>
+          <h2>Interested? Reach out now!</h2>
+          <base-button v-if="!isCoachDetailContactLink" link :to="contactLink">
+            Contact
+          </base-button>
+        </header>
+        <router-view></router-view>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <base-badge
+          v-for="area in areas"
+          :key="area"
+          :type="area"
+          :title="area"
+        >
+        </base-badge>
+        <p>{{ description }}</p>
+      </base-card>
+    </section>
+  </div>
 </template>
+
+<script lang='ts'>
+import { defineComponent } from "vue";
+import { ICoach } from "@/pages/coaches/types";
+
+export default defineComponent({
+  props: ["id"],
+  data() {
+    return {
+      selectedCoach: null as null | ICoach,
+    };
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach?.lastName + " " + this.selectedCoach?.firstName;
+    },
+    contactLink() {
+      return this.$route.path + "/contact";
+    },
+    description() {
+      return this.selectedCoach?.description;
+    },
+    rate() {
+      return this.selectedCoach?.hourlyRate;
+    },
+    areas() {
+      return this.selectedCoach?.areas;
+    },
+    isCoachDetailContactLink() {
+      const matchedPath = this.$route.matched;
+      if (matchedPath.length > 1) {
+        return matchedPath[1]?.path === "/coaches/:id/contact";
+      } else {
+        return false;
+      }
+    },
+  },
+
+  created() {
+    this.selectedCoach = this.$store.getters["coaches/coaches"].find(
+      (coach: ICoach) => coach.id === this.id
+    );
+  },
+});
+</script>
