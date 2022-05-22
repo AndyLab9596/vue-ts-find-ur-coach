aac60ca5231858aea708e8c2139b86e27efa5aa2
andyvy <thienvy95@gmail.com>
2022-05-22 09:52:29 +0700

    register components

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// components
+import BaseCard from '@/components/ui/BaseCard.vue';
+import BaseButton from '@/components/ui/BaseButton.vue';
+import BaseDialog from '@/components/ui/BaseDialog.vue';
+import BaseSpinner from '@/components/ui/BaseSpinner.vue';
+import BaseBadge from '@/components/ui/BaseBadge.vue';
+
+const app = createApp(App);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-dialog', BaseDialog);
+app.component('base-spinner', BaseSpinner);
+app.component('base-badge', BaseBadge);
+
+app.use(store);
+app.use(router);
+
+
+app.mount('#app')
