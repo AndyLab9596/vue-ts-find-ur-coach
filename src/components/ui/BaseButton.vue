28b5301b00bbeabaa89560ba10ecd6f08ca814b1
andyvy <thienvy95@gmail.com>
2022-05-22 09:45:15 +0700

    create BaseButton

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,73 @@
+<template>
+    <button v-if="!link" :class="mode">
+        <slot></slot>
+    </button>
+    <router-link v-else :to="to" :class="mode">
+        <slot></slot>
+    </router-link>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        link: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+        to: {
+            type: String,
+            required: false,
+            default: "/",
+        },
+        mode: {
+            type: String,
+            required: false,
+            default: null,
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+button,
+a {
+    text-decoration: none;
+    padding: 0.75rem 1.5rem;
+    background-color: #3a0061;
+    border: 1px solid #3a0061;
+    color: #ffffff;
+    cursor: pointer;
+    border-radius: 30px;
+    margin-right: 0.5rem;
+    display: inline-block;
+
+    &:hover,
+    &:active {
+        background-color: #270041;
+        border-color: #270041;
+    }
+}
+
+.flat {
+    background-color: transparent;
+    color: #3a0061;
+    border: none;
+
+    &:hover,
+    &:active {
+        background-color: #edd2ff;
+    }
+}
+
+.outline {
+    background-color: transparent;
+    border-color: #270041;
+    color: #270041;
+
+    &:hover,
+    &:active {
+        background-color: #edd2ff;
+    }
+}
+</style>
