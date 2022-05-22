fbb4e4a4a003275ddcf5d0b4282a20935c5afc08
andyvy <thienvy95@gmail.com>
2022-05-22 09:37:12 +0700

    create NotFound page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3>
+            Not Found
+        </h3>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+    display: grid;
+    place-items: center;
+    max-width: 20rem;
+    width: 100%;
+    margin: auto;
+    text-align: center;
+
+    h3 {
+        font-size: 2.5rem;
+        color: #c3c3c3;
+    }
+}
+</style>
+
