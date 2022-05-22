9611825cf9d2300bd6330d3f7928c2f24e9113cf
andyvy <thienvy95@gmail.com>
2022-05-22 09:29:46 +0700

    create navbar and setup vuex auth module

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,75 @@
 <template>
-  <h3>Hello world</h3>
+  <the-header></the-header>
+  <!-- <router-view v-slot="slotProps">
+    <transition name="route" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view> -->
 </template>
+
+<script lang="ts">
+import TheHeader from "./components/layout/TheHeader.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    TheHeader,
+  },
+  created() {
+    this.$store.dispatch("tryLogin");
+  },
+  computed: {
+    didAutoLogout() {
+      return this.$store.getters.didAutoLogout;
+    },
+  },
+  watch: {
+    didAutoLogout(currValue, newValue) {
+      if (currValue && currValue !== newValue) {
+        this.$router.replace("/coaches");
+      }
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
--- src/store/modules/auth/actions.ts
+++ src/store/modules/auth/actions.ts
@@ -1,9 +1,107 @@
+import config from "@/config";
 import { RootState } from "@/store/types";
 import { ActionTree } from "vuex";
 import { AuthState } from "./types";
 
-const actions: ActionTree<AuthState, RootState> = {
+let timer: number | undefined;
 
+const actions: ActionTree<AuthState, RootState> = {
+    async login(context, payload) {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
+        })
+    },
+    async signup(context, payload) {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        })
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        let url;
+        if (mode === 'login') {
+            url = config.SIGN_IN_URL
+        } else {
+            url = config.SIGN_UP_URL
+        }
+
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: payload.email,
+                password: payload.password,
+                returnSecureToken: true
+            })
+        })
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to authenticated');
+            throw error;
+        }
+
+        const expiresIn = +responseData.expiresIn * 1000;
+        const expirationDate = new Date().getTime() + expiresIn;
+
+        localStorage.setItem('token', responseData.idToken);
+        localStorage.setItem('userId', responseData.localId);
+        localStorage.setItem('tokenExpiration', expirationDate.toString());
+
+        timer = setTimeout(() => {
+            // Dispatch auto logout
+            context.dispatch('autoLogout')
+        }, expiresIn)
+
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId
+        })
+    },
+
+    async tryLogin(context) {
+        const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
+        const tokenExpiration = localStorage.getItem('tokenExpiration') as string;
+
+        const expiresIn = +tokenExpiration - new Date().getTime();
+
+        if (expiresIn < 0) {
+            return;
+        }
+
+        timer = setTimeout(() => {
+            // Dispatch auto logout
+            context.dispatch('autoLogout')
+        }, expiresIn)
+
+        if (token && userId) {
+            context.commit('setUser', {
+                token,
+                userId,
+            })
+        }
+    },
+
+    logout(context) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userId');
+        localStorage.removeItem('tokenExpiration');
+
+        clearTimeout(timer);
+
+        context.commit('setUser', {
+            token: null,
+            userId: null,
+        })
+    },
+
+    autoLogout(context) {
+        context.dispatch('logout');
+        context.commit('setAutoLogout')
+    }
 }
 
 export default actions;
