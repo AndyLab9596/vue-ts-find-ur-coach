e3be0f4681e2706f318e210dc0168acbd76ee57f
andyvy <thienvy95@gmail.com>
2022-05-22 15:47:59 +0700

    fixing not register vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,12 @@ import { createStore } from 'vuex'
 import { RootState } from './types'
 import authModule from './modules/auth';
 import coachesModule from './modules/coaches';
+import requestsModule from './modules/requests';
 
 export default createStore<RootState>({
   modules: {
     auth: authModule,
-    coaches: coachesModule
+    coaches: coachesModule,
+    requests: requestsModule
   }
 })
